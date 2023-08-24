7b39a8e3956a0e4dbf51c09a2d905c5a6b71512b
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-08-24 17:17:55 +0100

    Day 6 Challenge

---
+++ Day6.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/?envType=study-plan-v2&envId=30-days-of-javascript
+
+const filter = function (arr, fn) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+console.log(
+  filter(
+    [0, 10, 20, 30],
+    (fn = function greaterThan10(n) {
+      return n > 10;
+    })
+  )
+); // [20, 30]
+
+console.log(
+  filter(
+    [1, 2, 3],
+    (fn = function firstIndex(n, i) {
+      return i === 0;
+    })
+  )
+);
